0ed84c617155894f358d6105b87e9d976ca3434d
Sagar Patel <sagar.patel19nov@gmail.com>
2024-12-30 14:06:57 -0500

    minor changes

--- backend/src/main/java/com/Revature/RevStay/models/User.java
+++ backend/src/main/java/com/Revature/RevStay/models/User.java
@@ -1,9 +1,30 @@
 package com.Revature.RevStay.models;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 @Entity
-@Table(name= "users")
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int user_id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String phone;
+
+    @Column(nullable = false)
+    private String role;
+
+
+    
 }

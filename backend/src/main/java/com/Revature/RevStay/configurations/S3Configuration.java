3210ed085cd3cc0cba0f40645e6e489e3115d183
Nathanael Gutierrez <nathanaelg16@gmail.com>
2024-12-31 15:26:18 -0500

    finishing pdf invoice function

--- backend/src/main/java/com/Revature/RevStay/configurations/S3Configuration.java
+++ backend/src/main/java/com/Revature/RevStay/configurations/S3Configuration.java
@@ -2,13 +2,13 @@ package com.Revature.RevStay.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3Configuration {
     @Bean
-    public S3AsyncClient buildS3AsyncClient() {
-        return S3AsyncClient
+    public S3Client buildS3Client() {
+        return S3Client
                 .builder()
                 .build();
     }

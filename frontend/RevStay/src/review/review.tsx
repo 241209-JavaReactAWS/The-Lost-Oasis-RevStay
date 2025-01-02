eb782f28ab549c81b99c0e1b9927552c96d34d6b
roland-a <177896797+rolandoarias12@users.noreply.github.com>
2025-01-02 17:09:03 -0500

    Updated review pop up
    Improved the design
    Added exit out functionality with fading out animation

--- frontend/RevStay/src/review/review.tsx
+++ frontend/RevStay/src/review/review.tsx
@@ -1,22 +1,24 @@
 import { useState } from 'react'
 
+import "./review.css"
+
 export default function Review(){
     const [stars, setStars] = useState(0)
+    const [isHidden, setHidden] = useState(false)
 
-    return <div>
-        <form>
-            <label>Stars:</label>
-            <div>
-                <Star starId={1} stars={stars} setStars={setStars}/>
-                <Star starId={2} stars={stars} setStars={setStars}/>
-                <Star starId={3} stars={stars} setStars={setStars}/>
-                <Star starId={4} stars={stars} setStars={setStars}/>
-                <Star starId={5} stars={stars} setStars={setStars}/>
-            </div>
-            <label>Description:</label><br/>
-            <input type="text" name="description"/><br/>
-            <input type="submit" value="Submit"/><br/>
-        </form>
+    return <div id="review" className= {isHidden ? "hidden" : ""}>
+        <Close setHidden={()=>setHidden(true)}/>
+        <label>Stars</label>
+        <div id="stars">
+            <Star starId={1} stars={stars} setStars={setStars}/>
+            <Star starId={2} stars={stars} setStars={setStars}/>
+            <Star starId={3} stars={stars} setStars={setStars}/>
+            <Star starId={4} stars={stars} setStars={setStars}/>
+            <Star starId={5} stars={stars} setStars={setStars}/>
+        </div>
+        <label>Description</label>
+        <textarea/>
+        <input type="submit" value="Submit"/>
     </div>
 }
 
@@ -37,5 +39,14 @@ function Star(props: StarProps){
         >
             {(props.starId <= props.stars)? "★" : "☆"}
         </span>
+    )
+}
+
+type closeProps = {
+    setHidden: ()=>void
+}
+function Close(props: closeProps){
+    return (
+        <div className="close" onClick={props.setHidden}>x</div>
     )
 }

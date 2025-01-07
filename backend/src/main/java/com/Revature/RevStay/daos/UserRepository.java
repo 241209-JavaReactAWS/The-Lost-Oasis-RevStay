a8a5d171e7d55467a04930af21fc3029eb4803df
Yurochkina <olga.yurochkina@gmail.com>
2025-01-07 16:05:00 -0500

    Resolving merge conflict.

--- backend/src/main/java/com/Revature/RevStay/daos/UserRepository.java
+++ backend/src/main/java/com/Revature/RevStay/daos/UserRepository.java
@@ -11,8 +11,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
 
-<<<<<<< HEAD
-=======
     Optional<User> findUserByEmail(String email);
->>>>>>> 69ad766 (Added hotel service and controller.)
+
 }

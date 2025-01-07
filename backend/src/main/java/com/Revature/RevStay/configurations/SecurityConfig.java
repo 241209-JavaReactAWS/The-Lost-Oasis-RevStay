e52a51f59c5a613dae4bea990ff8ed668253dee6
Nathanael Gutierrez <nathanaelg16@gmail.com>
2025-01-07 14:56:50 -0500

    fixing CORS error

--- backend/src/main/java/com/Revature/RevStay/configurations/SecurityConfig.java
+++ backend/src/main/java/com/Revature/RevStay/configurations/SecurityConfig.java
@@ -4,9 +4,11 @@ import com.Revature.RevStay.filter.JWTFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -28,8 +30,11 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable()) // Disable CSRF
+        http
+                .cors(Customizer.withDefaults()) // Enable CORS
+                .csrf(csrf -> csrf.disable()) // Disable CSRF
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // Allow preflight requests
                         .requestMatchers("/register", "/login").permitAll() // Allow unauthenticated access
                         .anyRequest().authenticated()) // Secure other endpoints
                 .sessionManagement(session -> session

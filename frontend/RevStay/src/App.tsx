8d95a05d9943fed114dab05d41b204ee3ba8dc6a
Nathanael Gutierrez <nathanaelg16@users.noreply.github.com>
2025-01-08 12:07:53 -0500

    showing hotel description page and room listings (#20)

--- frontend/RevStay/src/App.tsx
+++ frontend/RevStay/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import {BrowserRouter, Route, Routes} from 'react-router'
 import Navbar from './components/navbar/Navbar.tsx'
 import Notifications from './views/notification/Notifications.tsx'
+import Hotel from './views/hotel/Hotel.tsx'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Navbar />
         <Routes>
             <Route path='/notifications' element={<Notifications />} />
+            <Route path='/hotel/:id' element={<Hotel />} />
         </Routes>
       </BrowserRouter>
     </div>

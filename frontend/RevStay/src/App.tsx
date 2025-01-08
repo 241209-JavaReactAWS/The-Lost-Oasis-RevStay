cff85f6f62bdda6a16857be8b75423b6f782de7c
Yurochkina <olga.yurochkina@gmail.com>
2025-01-08 12:05:16 -0500

    React components for rooms and hotels for admin.

--- frontend/RevStay/src/App.tsx
+++ frontend/RevStay/src/App.tsx
@@ -1,7 +1,9 @@
 import './App.css'
-import {BrowserRouter, Route, Routes} from 'react-router'
+import { BrowserRouter, Route, Routes } from 'react-router'
 import Navbar from './components/navbar/Navbar.tsx'
 import Notifications from './views/notification/Notifications.tsx'
+import Rooms from './components/rooms/Rooms.tsx'
+import AdminHotelList from './views/admin-hotels/AdminHotelList.tsx'
 
 function App() {
   return (
@@ -9,7 +11,9 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-            <Route path='/notifications' element={<Notifications />} />
+          <Route path='/notifications' element={<Notifications />} />
+          <Route path='admin/hotels/:hotelId/rooms' element={<Rooms />} />
+          <Route path='/admin/hotels' element={<AdminHotelList />} />
         </Routes>
       </BrowserRouter>
     </div>

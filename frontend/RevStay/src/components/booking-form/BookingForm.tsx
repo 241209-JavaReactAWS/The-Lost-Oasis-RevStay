f7d72ee937eb97c3500372d6c67c24b8a4d4f8dc
Nathanael Gutierrez <nathanaelg16@gmail.com>
2025-01-09 13:54:58 -0500

    adding booking form, and changing from using  RequestAttribute to using AuthenticationPrincipal

--- frontend/RevStay/src/components/booking-form/BookingForm.tsx
+++ frontend/RevStay/src/components/booking-form/BookingForm.tsx
@@ -5,6 +5,7 @@ import IRoom from '../room/IRoom.tsx'
 import {Dayjs} from 'dayjs'
 import {postman} from '../../postman.ts'
 import { useNavigate } from 'react-router';
+
 interface Props {
     hotelId: string | undefined,
     hotel: {
@@ -27,7 +28,7 @@ export default function BookingForm(props: Props) {
             return;
         }
         const totalAmount = checkOutDate.diff(checkInDate, 'days') * props.room.pricePerNight;
-        
+
          postman
             .post('/bookings', {
                 hotelID: props.hotelId,
@@ -38,14 +39,14 @@ export default function BookingForm(props: Props) {
              })
              .then((res) => {
                 console.log(res);
-                navigate('/payment', { 
-                    state: { 
+                navigate('/payment', {
+                    state: {
                         hotelId: props.hotelId,
                             hotelName: props.hotel.name, // Pass the hotel name
                             room: props.room,
-                            userId: 1, 
+                            userId: 1,
                             totalAmount: props.room.pricePerNight * checkOutDate.diff(checkInDate, 'days'), // Calculate total price
-                    } 
+                    }
                 });
             })
             .catch((err) => {
@@ -57,44 +58,36 @@ export default function BookingForm(props: Props) {
     const reserveButtonDisabled = checkInDate === null || checkOutDate === null
         || checkInDate.isSame(checkOutDate) || checkInDate.isAfter(checkOutDate)
 
-        return (
-            <Box sx={{ my: 3 }}>
-                <Typography variant='h4'>Reservation</Typography>
-                <Typography variant='h5' sx={{ my: 2 }}>{props.room.roomType}</Typography>
-                <Stack direction='row' gap={3} sx={{ my: 2 }} alignItems='end'>
-                    <Box>
-                        <Typography>Check-in</Typography>
-                        <DatePicker value={checkInDate} onChange={(nv) => setCheckInDate(nv)} />
-                    </Box>
-                    <Box>
-                        <Typography>Check-out</Typography>
-                        <DatePicker value={checkOutDate} onChange={(nv) => setCheckOutDate(nv)} />
-                    </Box>
-                    <Box>
-                        <Typography>Guests</Typography>
-                        <Select label='Guests' value={numGuests} onChange={(e) => setNumGuests(e.target.value as number)}>
-                            <MenuItem value={1}>1</MenuItem>
-                            <MenuItem value={2}>2</MenuItem>
-                            <MenuItem value={3}>3</MenuItem>
-                            <MenuItem value={4}>4</MenuItem>
-                            <MenuItem value={5}>5</MenuItem>
-                            <MenuItem value={6}>6</MenuItem>
-                        </Select>
-                    </Box>
-                    <Box>
-                        <Typography>Total</Typography>
-                        <Box sx={{ py: 2, px: 1, border: '1px solid #0000003F' }}>
-                            <Typography>{(checkInDate && checkOutDate) ? `$${props.room.pricePerNight * checkOutDate.diff(checkInDate, 'days')}` : 'N/A'}</Typography>
-                        </Box>
-                    </Box>
-                    <Button 
-                        disabled={reserveButtonDisabled} 
-                        sx={{ height: 60 }} 
-                        variant='contained' 
-                        onClick={reserve}>
-                        Reserve
-                    </Button>
-                </Stack>
+    return <Box sx={{my: 3}}>
+        <Typography variant='h4'>Reservation</Typography>
+        <Typography variant='h5' sx={{my: 2}}>{props.room.roomType}</Typography>
+        <Stack direction='row' gap={3} sx={{my: 2}} alignItems='end'>
+            <Box>
+                <Typography>Check-in</Typography>
+                <DatePicker value={checkInDate} onChange={(nv) => setCheckInDate(nv)} />
             </Box>
-        )
+            <Box>
+                <Typography>Check-out</Typography>
+                <DatePicker value={checkOutDate} onChange={(nv) => setCheckOutDate(nv)} />
+            </Box>
+            <Box>
+                <Typography>Guests</Typography>
+                <Select label='Guests' value={numGuests} onChange={(e) => setNumGuests(e.target.value as number)}>
+                    <MenuItem value={1}>1</MenuItem>
+                    <MenuItem value={2}>2</MenuItem>
+                    <MenuItem value={3}>3</MenuItem>
+                    <MenuItem value={4}>4</MenuItem>
+                    <MenuItem value={5}>5</MenuItem>
+                    <MenuItem value={6}>6</MenuItem>
+                </Select>
+            </Box>
+            <Box>
+                <Typography>Total</Typography>
+                <Box sx={{py: 2, px: 1, border: '1px solid #0000003F'}}>
+                    <Typography>{(checkInDate && checkOutDate) ? `$${props.room.pricePerNight * checkOutDate.diff(checkInDate, 'days')}` : 'N/A'}</Typography>
+                </Box>
+            </Box>
+            <Button disabled={reserveButtonDisabled} sx={{height: 60}} variant='contained' onClick={reserve}>Reserve</Button>
+        </Stack>
+    </Box>
 }

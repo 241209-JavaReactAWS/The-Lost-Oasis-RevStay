81b0227eedd98dc23572e2cb491e002f83305e3f
Sagar Patel <sagar.patel19nov@gmail.com>
2025-01-09 16:08:01 -0500

    front end updates

--- frontend/RevStay/src/App.tsx
+++ frontend/RevStay/src/App.tsx
@@ -4,8 +4,8 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import Navbar from './components/navbar/Navbar.tsx'
 import Notifications from './views/notification/Notifications.tsx'
-import LoginPage from './views/notification/LoginPage.tsx'
-import RegisterPage from './views/notification/RegisterPage.tsx'
+import LoginPage from './views/login/LoginPage.tsx'
+import RegisterPage from './views/login/RegisterPage.tsx'
 import Rooms from './components/rooms/Rooms.tsx'
 import AdminHotelList from './views/admin-hotels/AdminHotelList.tsx'
 import Hotel from './views/hotel/Hotel.tsx'

ed67c2af47b7ccc4d5539630b6a192b19c6a70ef
Nathanael Gutierrez <nathanaelg16@users.noreply.github.com>
2025-01-11 14:56:37 -0500

    Implementing ability to manage user reservations (#38)
    * manage user reservations (frontend)
    
    * manage user reservations (backend)
    
    * fixing auth bug

--- frontend/RevStay/src/hooks/AuthProvider.tsx
+++ frontend/RevStay/src/hooks/AuthProvider.tsx
@@ -1,5 +1,6 @@
-import {ReactNode, useState} from 'react'
+import {ReactNode, useEffect, useState} from 'react'
 import { AuthContext } from './AuthContext'
+import {postman} from '../postman.ts'
 
 interface AuthProviderProps {
     children ?: ReactNode,
@@ -8,6 +9,17 @@ interface AuthProviderProps {
 export function AuthProvider({ children }: AuthProviderProps) {
     const [isAuthenticated, setAuthenticated] = useState<boolean>(false)
     const [role, setRole] = useState<'DEFAULT' | 'CUSTOMER' | 'OWNER'>('DEFAULT')
+
+    useEffect(() => {
+        if (sessionStorage.getItem('token')) {
+            postman.post('/validate-token')
+                .then((res) => {
+                    const user = res.data.user
+                    setAuthenticated(true)
+                    setRole(user.role)
+                })
+        }
+    }, [])
 
     return <AuthContext.Provider value={{
         isAuthenticated,

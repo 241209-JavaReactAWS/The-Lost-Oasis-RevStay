e5d300f531f917417aa7f3ad5b19414cad1299ce
roland-a <177896797+rolandoarias12@users.noreply.github.com>
2025-01-11 19:44:28 -0500

    Added getUserByAuthentification method

--- backend/src/main/java/com/Revature/RevStay/services/UserService.java
+++ backend/src/main/java/com/Revature/RevStay/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -84,5 +85,15 @@ public class UserService {
                 .orElseThrow(() -> new RuntimeException("User not found with username: " + email));
     }
 
+    public Optional<User> getUserByAuthentication(){
+        var auth = SecurityContextHolder.getContext().getAuthentication();
 
+        if (!auth.isAuthenticated()){
+            return Optional.empty();
+        }
+
+        var email = auth.getName();
+
+        return Optional.of(userRepository.findByEmail(email));
+    }
 }
